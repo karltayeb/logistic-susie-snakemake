3c6afb380d5bbc06f1ff6ddc51cf11569e37c0d2
karltayeb <karl.tayeb7@gmail.com>
2023-09-18 13:31:13 -0500

    simulation manifests

---
+++ config/gsea_sim/make_sim_manfist.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+manifest <- tidyr::crossing(
+    X = c('X_bin_strong'),
+    rep = 1:10,
+    beta0 = c(-5, -4, -3, -2),
+    beta = c(0.5, 1., 2., 4.),
+    q = c(1, 3, 5)) %>%
+  rowwise() %>%
+  mutate(
+    sim_hash = rlang::hash(c(X, rep, beta0, beta, q)),
+    sim_id = stringr::str_extract(sim_hash, "^.{5}"),
+    seed = strtoi(sim_id, 16)
+  )
+
+manifest %>% readr::write_delim('config/gsea_sim/gsea_constant_manifest.tsv', delim='\t')
+
+manifest %>% readr::write_delim('config/gsea_sim/gsea_sim_manifest.tsv', delim='\t')
